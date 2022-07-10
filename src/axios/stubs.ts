dffcab2b9554a8d309d537fcf6d740845ab071f7
Andrei Ledovskikh <auledovskih@gmail.com>
2022-07-10 20:15:12 +0400

    fix serialisation error in stub

--- src/axios/stubs.ts
+++ src/axios/stubs.ts
@@ -10,31 +10,31 @@ export const axiosStub = {
     console.log("STUB:", "params=", config.params)
 
     return {
-      data: new class implements ServerResponse<IAirport> {
-        count = 1101;
-        next = 12;
-        previous = 10;
-        results = [
-          new class implements IAirport {
-            id = 1
-            name = `"Der Dingel" Airfiled`
-            ident = `DE-0140`
-            local_code = `DE`
-            region = `DE-HE`
-            type = `small_airport`
-            country = `German`
-          }(),
-          new class implements IAirport {
-            id = 2
-            name = `‘S-Gravenvoeren heliport`
-            ident = `EBSN`
-            local_code = `BE`
-            region = `BE-WLG`
-            type = `closed`
-            country = `Belarus`
-          }()
-        ];
-      }()
+      data: {
+        count: 1101,
+        next: 12,
+        previous: 10,
+        results: [
+          {
+            id: 1,
+            name: '"Der Dingel" Airfiled',
+            ident: 'DE-0140',
+            local_code: 'DE',
+            region: 'DE-HE',
+            type: 'small_airport',
+            country: 'German'
+          } as IAirport,
+          {
+            id: 12,
+            name: '‘S-Gravenvoeren heliport',
+            ident: 'EBSN',
+            local_code: 'BE',
+            region: 'BE-WLG',
+            type: 'closed',
+            country: 'Belarus'
+          } as IAirport
+        ]
+      } as ServerResponse<IAirport>
     }
   }
 }
